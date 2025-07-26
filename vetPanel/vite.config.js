f13186cbf9cc8e6c349be0641cb672493164df11
Tejashri-Shirsath <tejjushrsath15@gmail.com>
2025-07-26 11:08:20 +0530

    updation for vercel.json for refresh the page

--- vetPanel/vite.config.js
+++ vetPanel/vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // 👈 This line is crucial for routing to work on Render
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // 👈 VERY IMPORTANT for Render deployment
+    host: '0.0.0.0',
     port: 3000
   }
 })
